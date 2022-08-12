7233bf5be8844bc5899d224f4ce62dda75cb5ec7
sjoerd-bouma <sjoerdbouma@xs4all.nl>
2022-08-12 17:56:04 +0200

    added button to select last run / 24 hours

--- RNODataViewer/tabs/run_viewer.py
+++ RNODataViewer/tabs/run_viewer.py
@@ -14,11 +14,13 @@ import RNODataViewer.spectrogram.spectrogram_average_plot
 import RNODataViewer.noise_rms.noise_rms
 from file_list.run_stats import run_table
 import numpy as np
+from dash import callback_context
+from dash.exceptions import PreventUpdate
+
 
 data_provider_run = RNODataViewer.base.data_provider_root.RNODataProviderRoot()
 
 
-##app.title = 'RNO Data Browser'
 run_viewer_layout = html.Div([
         RNODataViewer.station_selection.station_selection.layout_run_browser,
         html.Div([html.Div([
@@ -29,9 +31,11 @@ run_viewer_layout = html.Div([
                 multi=True,
                 className='custom-dropdown')],style={'width':'100%'}),
         html.Div([
-            html.Button('open file', id='btn-open-file', className='btn btn-default')
-            ], className='input-group-btn', style={'vertical-align':'bottom'}),
-        ], className='input-group',style={'width':'90%'}),
+            html.Div([
+            html.Button('Most recent run', id='select-last-run', style={'display':'block', 'minWidth':'150px','margin-top':2,'margin-bottom':2}),
+            html.Button('Most recent 24 hours', id='select-last-24h', style={'display':'block','minWidth':'150px', 'margin-top':2,'margin-bottom':2}),
+            ], style={'vertical-align':'bottom'}),], className='flexi-element-1')
+        ], className='flexi-box',style={'width':'100%'}),
     html.Div([
         html.Div([
             RNODataViewer.noise_rms.noise_rms.layout
@@ -49,6 +53,30 @@ run_viewer_layout = html.Div([
         ], className='flexi-element-1')
     ], className='flexi-box')
 ])
+
+@app.callback(
+    Output('file-name-dropdown-2', 'value'),
+    [Input('select-last-run', 'n_clicks'),
+     Input('select-last-24h', 'n_clicks'),
+    ],
+    State('station-id-dropdown-single', 'value'),
+    prevent_initial_call=True
+
+)
+def select_runs_button(last_run, last_24h, stations, run_table=run_table):
+    trigger = callback_context.triggered[0]['prop_id'].split('.')[0]
+    tab = run_table.get_table()
+    station_mask = np.array([np.isin(s, stations) for s in tab.station], dtype=bool)
+    tab_selected = tab[station_mask]
+    tab_selected_sort = tab_selected.sort_values(by='mjd_last_event')
+    if trigger == 'select-last-run':
+        return [tab_selected_sort.iloc[-1].loc['filenames_root']]
+    elif trigger == 'select-last-24h':
+        last_mjd = tab_selected_sort.iloc[-1].loc['mjd_last_event']
+        tab_last_24h = tab_selected_sort.query('mjd_last_event>@last_mjd-1')
+        return tab_last_24h.filenames_root
+    else:
+        raise PreventUpdate
 
 @app.callback(Output('file-name-dropdown-2', 'options'),
               [Input('station-id-dropdown-single', 'value')])
