612e87c8ae761b2edb4f58639f009a1416067710
Sjoerd Bouma <sjoerdbouma@xs4all.nl>
2024-07-22 23:52:43 +0200

    remove accidental reference to non-updating run table

--- RNODataViewer/pages/event_viewer.py
+++ RNODataViewer/pages/event_viewer.py
@@ -30,9 +30,6 @@ data_folder = DATA_DIR
 # browser_provider.set_filetype(True)
 browser_provider = data_provider_event
 
-filename_table = RUN_TABLE.get_table().loc[:, ['station', 'run', 'filenames_root']].drop_duplicates(subset=['station', 'run'])
-filename_table = filename_table.set_index(['station', 'run']).sort_index()
-
 # trigger_hover_info = (
 #     "Which trigger fired for this event. Options are\n"
 #     "LT: the low-threshold trigger on the FLOWER board (deep trigger)\n"
@@ -293,9 +290,10 @@ def update_everything(
             station_id = station_values[current_station_i + 1]
 
     ### update run & run options
-    run_numbers = RUN_TABLE.get_table().query(
+    table_for_station = RUN_TABLE.get_table().query(
         'station==@station_id').sort_values(
-        by='mjd_last_event', ascending=False).run.values.astype(int)
+        by='mjd_last_event', ascending=False)
+    run_numbers = table_for_station.run.values.astype(int)
     run_options = [{'label':i, 'value':i} for i in run_numbers]
 
     try:
@@ -317,7 +315,7 @@ def update_everything(
         id='event-info-run', style={'flex':1}),
 
     ### update event & event options
-    filename = filename_table.loc[(station_id, run_number), 'filenames_root']
+    filename = table_for_station.loc[(station_id, run_number), 'filenames_root']
     number_of_events = browser_provider.get_file_handler(user_id, filename).get_n_events()
     event_ids = browser_provider.get_file_handler(user_id, filename).get_event_ids()
     event_id_options = [{'label':i[1], 'value':i[1]} for i in event_ids]
